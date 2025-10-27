58c96db10b271cfc56502ea906fbf1ef0e6f20d8
Tegar Kang Ageng Gilang <tulenggilang@gmail.com>
2025-10-27 23:21:14 +0700

    UP

--- 07_Navigasi_Notifikasi/unguided/lib/main.dart
+++ 07_Navigasi_Notifikasi/unguided/lib/main.dart
@@ -26,8 +26,7 @@ class BottomNavExample extends StatefulWidget {
 }
 
 class _BottomNavExampleState extends State<BottomNavExample> {
-  int _selectedIndex = 2; // Search default aktif
-  bool _isHoveringSearch = false;
+  int _selectedIndex = 2;
 
   final List<Widget> _pages = const [
     Center(child: Text('Halaman Home')),
@@ -46,8 +45,6 @@ class _BottomNavExampleState extends State<BottomNavExample> {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
       body: SafeArea(child: _pages[_selectedIndex]),
-
-      // ===== NAVBAR =====
       bottomNavigationBar: Container(
         height: 85,
         margin: const EdgeInsets.only(left: 25, right: 25, bottom: 20),
@@ -62,91 +59,51 @@ class _BottomNavExampleState extends State<BottomNavExample> {
             ),
           ],
         ),
-        child: Stack(
-          alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _navIcon(Icons.home_rounded, 0, "Home"),
-                _navIcon(Icons.edit_note_rounded, 1, "Aduan"),
-                const SizedBox(width: 70),
-                _navIcon(Icons.notifications_none_rounded, 3, "Notifikasi"),
-                _navIcon(Icons.person_outline_rounded, 4, "Profil"),
-              ],
-            ),
-            // Tombol Tengah (Search)
-            Positioned(
-              top: 8,
-              child: MouseRegion(
-                onEnter: (_) => setState(() => _isHoveringSearch = true),
-                onExit: (_) => setState(() => _isHoveringSearch = false),
-                child: GestureDetector(
-                  onTap: () => _onItemTapped(2),
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 200),
-                    height: 70,
-                    width: 70,
-                    decoration: BoxDecoration(
-                      color: _selectedIndex == 2
-                          ? const Color(0xFF023B3D)
-                          : (_isHoveringSearch
-                                ? const Color(0xFF02555A)
-                                : const Color(0xFF12676D)),
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 7),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.25),
-                          blurRadius: 9,
-                          offset: const Offset(0, 5),
-                        ),
-                      ],
-                    ),
-                    child: const Icon(
-                      Icons.search_rounded,
-                      color: Colors.white,
-                      size: 35,
-                    ),
+            _navIcon(Icons.home_rounded, 0),
+            _navIcon(Icons.edit_note_rounded, 1),
+            Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                color: const Color(0xFF023B3D),
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 7),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.25),
+                    blurRadius: 9,
+                    offset: const Offset(0, 5),
                   ),
-                ),
+                ],
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.search_rounded,
+                    color: Colors.white, size: 35),
+                onPressed: () => _onItemTapped(2),
               ),
             ),
+            _navIcon(Icons.notifications_none_rounded, 3),
+            _navIcon(Icons.person_outline_rounded, 4),
           ],
         ),
       ),
     );
   }
 
-  Widget _navIcon(IconData icon, int index, String label) {
+  Widget _navIcon(IconData icon, int index) {
     bool selected = _selectedIndex == index;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        IconButton(
-          onPressed: () => _onItemTapped(index),
-          icon: Icon(
-            icon,
-            color: selected ? Colors.white : Colors.white70,
-            size: selected ? 35 : 30,
-          ),
-        ),
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 11,
-            fontWeight: FontWeight.w500,
-            color: selected ? Colors.white : Colors.white70,
-          ),
-        ),
-      ],
+    return IconButton(
+      onPressed: () => _onItemTapped(index),
+      icon: Icon(icon,
+          color: selected ? Colors.white : Colors.white70,
+          size: selected ? 35 : 30),
     );
   }
 }
 
-//
-// ======= PAGE: UMKM DESA =======
-//
 class UMKMDesaPage extends StatelessWidget {
   const UMKMDesaPage({super.key});
 
@@ -155,25 +112,25 @@ class UMKMDesaPage extends StatelessWidget {
       'name': 'Oraumum',
       'address': 'Jl. Dr. Soeparno No. 98 Kembaran Wetan',
       'rating': 4.7,
-      'image': 'assets/alunalunpurwokerto.jpg',
+      'image': 'assets/satu.jpg',
     },
     {
       'name': 'Mampir Pawon',
       'address': 'Jl. Mawar No. 5 Kembaran Wetan',
       'rating': 4.8,
-      'image': 'assets/teratai.jpg',
+      'image': 'assets/dua.jpg',
     },
     {
       'name': 'Warung Mie Ramen',
       'address': 'Jl. Mojo Gede Kembaran Wetan',
       'rating': 4.5,
-      'image': 'assets/underpas.jpg',
+      'image': 'assets/tiga.jpg',
     },
     {
       'name': 'Roti Sehati',
       'address': 'Jl. Gayamsari Kembaran Wetan',
       'rating': 4.7,
-      'image': 'assets/store4.png',
+      'image': 'assets/empat.jpg',
     },
   ];
 
@@ -182,45 +139,45 @@ class UMKMDesaPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFF7F8FA),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 11),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Search Bar
+            // Search bar
             Row(
               children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.arrow_back_rounded),
-                ),
+                const Icon(Icons.arrow_back_rounded, size: 28),
+                const SizedBox(width: 10),
                 Expanded(
                   child: Container(
-                    height: 45,
+                    height: 55,
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(25),
+                      borderRadius: BorderRadius.circular(30),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black.withOpacity(0.05),
-                          blurRadius: 5,
+                          color: Colors.black.withOpacity(0.08),
+                          blurRadius: 8,
+                          offset: const Offset(0, 3),
                         ),
                       ],
                     ),
                     child: const TextField(
                       decoration: InputDecoration(
                         hintText: 'Pencarian Produk',
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
+                        prefixIcon:
+                            Icon(Icons.search, color: Colors.grey, size: 26),
                         border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(top: 10),
+                        contentPadding: EdgeInsets.only(top: 14),
                       ),
                     ),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 12),
 
-            // Scroll kategori
+            // Kategori
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
@@ -229,108 +186,101 @@ class UMKMDesaPage extends StatelessWidget {
                   CategoryChip(label: 'Makanan'),
                   CategoryChip(label: 'Minuman'),
                   CategoryChip(label: 'Keperluan'),
+                  CategoryChip(label: 'Peralatan'),
                   CategoryChip(label: 'Lainnya'),
                 ],
               ),
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 5),
 
-            // List UMKM scrollable
+            // List toko
             Expanded(
               child: ListView.builder(
                 itemCount: _umkmList.length,
                 itemBuilder: (context, index) {
                   final item = _umkmList[index];
-                  return GestureDetector(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => DetailUMKMPage(umkm: item),
-                      ),
+                  return Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
                     ),
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(vertical: 6),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.08),
-                            blurRadius: 6,
-                            offset: const Offset(0, 3),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(18),
+                            bottomLeft: Radius.circular(18),
                           ),
-                        ],
-                      ),
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.asset(
-                              item['image'],
-                              height: 70,
-                              width: 70,
-                              fit: BoxFit.cover,
-                            ),
+                          child: Image.asset(
+                            item['image'],
+                            height: 110,
+                            width: 110,
+                            fit: BoxFit.cover,
                           ),
-                          const SizedBox(width: 10),
-                          Expanded(
+                        ),
+                        const SizedBox(width: 14),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 4),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
                                   item['name'],
                                   style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w800,
                                   ),
                                 ),
                                 const SizedBox(height: 4),
                                 Text(
                                   item['address'],
                                   style: const TextStyle(
-                                    fontSize: 13,
+                                    fontSize: 14,
                                     color: Colors.black54,
                                   ),
+                                ),
+                                const SizedBox(height: 7),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.star,
+                                        color: Colors.amber, size: 20),
+                                    const SizedBox(width: 6),
+                                    Text(
+                                      item['rating'].toString(),
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w800,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      height: 32,
+                                      width: 32,
+                                      decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: const Icon(Icons.add,
+                                          color: Colors.white, size: 22),
+                                    ),
+                                    const SizedBox(width: 10),
+                                  ],
                                 ),
                               ],
                             ),
                           ),
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  const Icon(
-                                    Icons.star,
-                                    color: Colors.amber,
-                                    size: 18,
-                                  ),
-                                  Text(
-                                    item['rating'].toString(),
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 8),
-                              Container(
-                                height: 28,
-                                width: 28,
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: const Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                  size: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   );
                 },
@@ -343,8 +293,6 @@ class UMKMDesaPage extends StatelessWidget {
   }
 }
 
-//
-// ======= CATEGORY CHIP =======
 class CategoryChip extends StatelessWidget {
   final String label;
   const CategoryChip({super.key, required this.label});
@@ -353,45 +301,20 @@ class CategoryChip extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(right: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(25),
         boxShadow: [
-          BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 4),
+          BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 5),
         ],
       ),
       child: Text(
         label,
         style: const TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
           color: Colors.black87,
-        ),
-      ),
-    );
-  }
-}
-
-//
-// ======= DETAIL PAGE =======
-class DetailUMKMPage extends StatelessWidget {
-  final Map<String, dynamic> umkm;
-  const DetailUMKMPage({super.key, required this.umkm});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(umkm['name']),
-        backgroundColor: const Color(0xFFD62828),
-        foregroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Text(
-          "Selamat datang di ${umkm['name']}\nAlamat: ${umkm['address']}",
-          textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
         ),
       ),
     );
